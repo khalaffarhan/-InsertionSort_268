df618767bd50e479dc8d2bfb6c06e63d9c8dd83c
khalaffarhan <khalaffarhan54@gmail.com>
2026-03-13 14:41:52 +0700

    commit 4

--- InsertionSort_268.cpp
+++ InsertionSort_268.cpp
@@ -25,3 +25,19 @@ void input (){
     }
 }
 
+void insertionsort()
+{ // procedure insertion sort
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        
